8a22509ca5980514bdc8940fbc32faea4d1377d4
pwhited-7 <pacen-w77@hotmail.com>
2021-07-30 15:54:38 -0600

    Created method to insert data into database tables

--- src/Database.java
+++ src/Database.java
@@ -1,16 +1,51 @@
+import javax.xml.crypto.Data;
 import java.sql.*;
 
 public class Database {
 
+    private Connection connection = null;
+    private Statement statement = null;
 
 
-    Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/School_Database", "root", "password");
+    public void insertIntoTable(String table, String id, String name, String gradeLevel, String feesPaid, String feesTotal ){
 
-    Statement statement = connection.createStatement();
+        try{
 
-    ResultSet resultSet = statement.executeQuery("SELECT * FROM schoolDatabase");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/School_Database", "root", "rootpassword");
+
+            statement = connection.createStatement();
+
+            String dataToInsert = String.format("INSERT INTO %s VALUES(%s, \"%s\", %s, %s, %s)", table, id, name, gradeLevel, feesPaid, feesTotal);
+
+            System.out.println(dataToInsert);
+            statement.executeUpdate(dataToInsert);
+
+        }catch (SQLException se){
+            se.printStackTrace();
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            try{
+                if (statement != null)
+                    connection.close();
+            }catch (SQLException se){
+
+            }
+            try{
+                if (connection != null){
+                    connection.close();
+                }
+            }catch (SQLException se){
+                se.printStackTrace();
+            }
+        }
+    }
 
 
-    public Database() throws SQLException {
+    public static void main(String[] args) {
+
+        Database data = new Database();
+
+        data.insertIntoTable("students", "3", "Pacen", "6", "0" ,"5000");
     }
 }
